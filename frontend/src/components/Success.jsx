54b62239639aaa2be998e918afd4be076494821e
MuhammadElhajj <3muhammad.el.hajj@gmail.com>
2025-04-08 00:39:10 +0300

    Update Success.jsx

--- frontend/src/components/Success.jsx
+++ frontend/src/components/Success.jsx
@@ -4,7 +4,7 @@ function SuccessMessage({SetStatus , Status, successButton}) {
   return (
     // <div class="Success__Message">
     <div class={Status == "Email sent successfully!" ? "Success__Message" : "Failed__Message" }>
-      <h2>{Status == "Email sent successfully!" ? Success : Failed }</h2>
+      <h2>{Status == "Email sent successfully!" ? "Success" : "Failed" }</h2>
       <p>{Status}</p>
       <button onClick={successButton}>Ok</button>
     </div>
